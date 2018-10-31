5156c228e1dae41caa88c8ccfa00be0359c2b9e1
Pittendreigh <msp1026@student.keene.edu>
2018-10-30 20:47:31 -0400

    Fixed matlab specific bugs

--- loadData.m
+++ loadData.m
@@ -1,20 +1,21 @@
-#Load and save project path based on where this script exists
-projectDirectory = strcat(mfilename('fullpath'), '\..')
-#Change directory to the project
+%Load and save project path based on where this script exists
+projectDirectory = mfilename('fullpath');
+projectDirectory = projectDirectory(1:length(mfilename('fullpath'))-8);
+%Change directory to the project
 cd(projectDirectory);
 
-#Find and save the directory path dynamically for the DOG image set
+%Find and save the directory path dynamically for the DOG image set
 dogDirectory = (strcat(projectDirectory,'\dogSampleSet'));
-#Test to make sure the folder loaded properly
+%Test to make sure the folder loaded properly
 if ~isfolder(dogDirectory)
     errorMessage = sprintf('Error: The following folder does not exist:\n%s', dogDirectory);
     uiwait(warndlg(errorMessage));
     return;
 end
 
-#Find and save the directory path dynamically for the BEACH image set
+%Find and save the directory path dynamically for the BEACH image set
 beachDirectory = (strcat(projectDirectory,'\beachSampleSet'));
-#Test to make sure the folder loaded properly
+%Test to make sure the folder loaded properly
 if ~isfolder(beachDirectory)
     errorMessage = sprintf('Error: The following folder does not exist:\n%s', beachDirectory);
     uiwait(warndlg(errorMessage));
