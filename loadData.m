3a86da2a3841c5f7f454c5de610b43fc9e2e2001
matthew <m@example.com>
2018-11-05 22:10:28 -0500

    Fully implemented loading, cropping, resizing, and saving functionality

--- loadData.m
+++ loadData.m
@@ -13,15 +13,20 @@ if ~isfolder(dogDirectory)
     return;
 end
 
-dogFile = dir(fullfile(dogDirectory, 'name*.jpg'));
+dogFile = dir(fullfile(dogDirectory, '*.jpg'));
 totalDogImages = numel(dogFile);
 
-for n = 1:totalImages
-    f = fullfile(ImageFolder, fileName(n).name);
-    dogImages = imread(f);
-    figure(n);
-    imshow(dogImages);
+for n = 1:totalDogImages
+    d = fullfile(dogDirectory, dogFile(n).name);
+    dogImages = imread(d); %Reads the folder of dog images
+    [J, rect] = imcrop(dogImages); %Allows the user to crop(square) the images
+    resizedImg = imresize(J, [227 227]); %Change the image dimensions
+    
+    figure (n);
+    newDogFolder = fullfile(dogDirectory, sprintf('Dog #%d.png', n)); %Save the edited images
+    imwrite(resizedImg, newDogFolder);
 end
+
 
 %Find and save the directory path dynamically for the BEACH image set
 beachDirectory = (strcat(projectDirectory,'\beachSampleSet'));
@@ -31,4 +36,19 @@ if ~isfolder(beachDirectory)
     uiwait(warndlg(errorMessage));
     return;
 end
+
+beachFile = dir(fullfile(beachDirectory, '*.jpg'));
+totalBeachImages = numel(beachFile);
+
+for n = 1:totalBeachImages
+    b = fullfile(beachDirectory, beachFile(n).name);
+    beachImages = imread(b); %Reads the folder of beach images
+    [J, rect] = imcrop(beachImages); %Allows the user to crop(square) the images
+    resizedImg = imresize(J, [227 227]); %Change the image dimensions
+    
+    figure (n);
+    newBeachFolder = fullfile(beachDirectory, sprintf('Beach #%d.png', n)); %Save the edited images
+    imwrite(resizedImg, newBeachFolder);
+end
+
 
