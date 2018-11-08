daac2cf823d8f43014fe8dcbffe6ae4ff689a45c
Pittendreigh <msp1026@student.keene.edu>
2018-11-08 12:52:38 -0500

    Finished the initial image processing up to the point of saving a .mat file

--- loadData.m
+++ loadData.m
@@ -15,18 +15,17 @@ end
 
 dogFile = dir(fullfile(dogDirectory, '*.jpg'));
 totalDogImages = numel(dogFile);
+dogImgSet = zeros(100,51529);
 
 for n = 1:totalDogImages
     d = fullfile(dogDirectory, dogFile(n).name);
     dogImages = imread(d); %Reads the folder of dog images
     %[J, rect] = imcrop(dogImages); %Allows the user to crop(square) the images
     resizedImg = imresize(dogImages, [227 227]); %Change the image dimensions
-    
-    figure (n);
-    newDogFolder = fullfile(dogDirectory, sprintf('Dog #%d.png', n)); %Save the edited images
-    imwrite(resizedImg, newDogFolder);
+    greyScaleImg = rgb2gray(resizedImg);
+    unwrappedImg = greyScaleImg(:);
+    dogImgSet(n,:) = (unwrappedImg)';
 end
-
 
 %Find and save the directory path dynamically for the BEACH image set
 beachDirectory = (strcat(projectDirectory,'\beachSampleSet'));
@@ -39,16 +38,16 @@ end
 
 beachFile = dir(fullfile(beachDirectory, '*.jpg'));
 totalBeachImages = numel(beachFile);
+beachImgSet = zeros(100,51529);
 
 for n = 1:totalBeachImages
     b = fullfile(beachDirectory, beachFile(n).name);
     beachImages = imread(b); %Reads the folder of beach images
     %[J, rect] = imcrop(beachImages); %Allows the user to crop(square) the images
     resizedImg = imresize(beachImages, [227 227]); %Change the image dimensions
-    
-    figure (n);
-    newBeachFolder = fullfile(beachDirectory, sprintf('Beach #%d.png', n)); %Save the edited images
-    imwrite(resizedImg, newBeachFolder);
+    greyScaleImg = rgb2gray(resizedImg);
+    unwrappedImg = greyScaleImg(:);
+    beachImgSet(n,:) = (unwrappedImg)';
 end
 
-
+save('datafile','dogImgSet','beachImgSet');
