d391b9cd023b0fabbe480584ca4c97122dff5bbf
Matthew Pittendreigh <mattpitt1998@gmail.com>
2018-11-14 17:20:17 -0500

    Removed bloat and made all octave scripts matlab compatible (I hope!)

--- costFunctionReg.m
+++ costFunctionReg.m
@@ -7,31 +7,24 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
 J = 0;
 grad = zeros(size(theta));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
-
-    #compute predictions
+    %compute predictions
     prediction = sigmoid(X * theta);
     
     #regularization component
     regOffset = (lambda/(2*m)) * sum(theta(2:size(theta)) .^ 2);
 
-    #compute cost and return
+    %compute cost and return
     J = (1/m) * sum(-y .* log(prediction) - (1-y) .* log(1 - prediction)) + regOffset;
     
-    #compare the predicted value with the empirical value in 
+    %compare the predicted value with the empirical value in 
     error = (prediction - y);
     
-    #compute gradient
+    %compute gradient
     grad = (1/m) * sum((X' * error),2) + (lambda/m).*theta;
-    #adjust theta zero
+    %adjust theta zero
     grad(1) = grad(1) - (lambda/m)*theta(1);
 
 % =============================================================
